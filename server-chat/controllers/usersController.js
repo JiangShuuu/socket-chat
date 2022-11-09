6acc16d614266e6036ea256bd13a1dda764e09f9
JiangShuuu <globelex65@gmail.com>
2022-11-09 22:48:17 +0800

    add deleteUser

--- server-chat/controllers/usersController.js
+++ server-chat/controllers/usersController.js
@@ -14,7 +14,24 @@ module.exports.creatUser = async (req, res, next) => {
 
     const user = await User.create({ userId })
 
-    return res.json({ status: true, user })
+    return res.json({ status: true, connectId: user.userId })
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+}
+
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const { userId } = req.body
+    User.findOne({ userId })
+      .then(user => {
+        if (!user) {
+          return res.json({ status: 404, err:'找不到使用者！' })
+        }
+        user.delete()
+      })
+    return res.json({ status: true, err:'使用者已刪除！' })
   } catch (error) {
     console.error(error)
     next(error)
--- server-chat/routes/userRoutes.js
+++ server-chat/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { creatUser } = require("../controllers/usersController")
+const { creatUser, deleteUser } = require("../controllers/usersController")
 
 const router = require("express").Router()
 
 router.post("/user", creatUser)
+router.delete("/user", deleteUser)
 
 module.exports = router
