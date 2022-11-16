aab122362831ebbaee3dc143f6ec8c4e077a8b9f
JiangShuuu <globelex65@gmail.com>
2022-11-16 20:38:29 +0800

    add prettier

---
+++ client-chat/src/hooks/useSocket.js
@@ -0,0 +1,16 @@
+import { useEffect, useRef } from 'react'
+import io from 'socket.io-client'
+
+export const useSocket = (url, options) => {
+  const { current: socket } = useRef(io(url, options))
+
+  useEffect(() => {
+    return () => {
+      if (socket) {
+        socket.close()
+      }
+    }
+  }, [socket])
+
+  return socket
+}
