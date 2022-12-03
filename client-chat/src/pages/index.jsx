c4846b7f8183c84526b2be9e4e1803f54ecdbeda
JiangShuuu <globelex65@gmail.com>
2022-12-03 20:37:23 +0800

    upd css

--- client-chat/src/pages/index.jsx
+++ client-chat/src/pages/index.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Chat from '../components/Chat'
 import { v4 as uuidv4 } from 'uuid'
 import { SocketProvider, useSocketContext } from '../context/SocketContext'
-import { WebRtcProvider, useWebRtcContext } from '../context/WebRtcContext'
+import { WebRtcProvider } from '../context/WebRtcContext'
 import userAPI from '../apis/user'
 import VideoPlayer from '../components/VideoPlayer'
 
@@ -18,15 +18,7 @@ function Index() {
     setEnd,
     setTalker,
   } = useSocketContext()
-  const {
-    call,
-    callAccepted,
-    userVideo,
-    callEnded,
-    callUser,
-    leaveCall,
-    answerCall,
-  } = useWebRtcContext()
+
   const connectSocket = async () => {
     // 開啟並連線
     toggleMenu(true)
